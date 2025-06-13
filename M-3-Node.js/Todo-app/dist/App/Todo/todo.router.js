4656429c1af291c6e1164208152f050bd9340c01
H.M Sadiq <hmsadikurrahman120@gmail.com>
2025-06-13 19:51:21 +0600

    added new router

--- M-3-Node.js/Todo-app/dist/App/Todo/todo.router.js
+++ M-3-Node.js/Todo-app/dist/App/Todo/todo.router.js
@@ -6,6 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.todosRouter = void 0;
 const express_1 = __importDefault(require("express"));
 exports.todosRouter = express_1.default.Router();
+// GET all todos
 exports.todosRouter.get('/', (req, res) => {
-    res.send('Hello World!333345');
+    res.json({ "name": "HTML5" });
+});
+// CREATE a new todo
+exports.todosRouter.post('/create-todo', (req, res) => {
+    res.json({ "name": "HTML5" });
+});
+// UPDATE  an existing todo
+exports.todosRouter.put('/update-todo', (req, res) => {
+    res.json({ "name": "HTML5" });
 });
--- M-3-Node.js/Todo-app/src/App/Todo/todo.router.ts
+++ M-3-Node.js/Todo-app/src/App/Todo/todo.router.ts
@@ -1,6 +1,17 @@
 import express, {  Request, Response } from 'express';
 export let todosRouter = express.Router();
  
+// GET all todos
 todosRouter.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!333345')
+  res.json({"name": "HTML5"})
+})
+
+// CREATE a new todo
+todosRouter.post('/create-todo', (req:Request, res:Response) => {
+  res.json({"name": "HTML5"})
+})
+
+// UPDATE  an existing todo
+todosRouter.put('/update-todo', (req:Request, res:Response) => {
+  res.json({"name": "HTML5"})
 })
