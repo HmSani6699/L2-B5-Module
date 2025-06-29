6177ab8ed3eaf2e22ea6066349d7ad0f801a8a67
H.M Sadiq <hmsadikurrahman120@gmail.com>
2025-06-29 23:22:52 +0600

    practice mongoose assignmment

--- M-3-Node.js/Todo-app/dist/db/config.js
+++ M-3-Node.js/Todo-app/dist/db/config.js
@@ -2,7 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.client = void 0;
 const mongodb_1 = require("mongodb");
-const uri = "mongodb+srv://mongodb:%40devsadiq6699@cluster0.2wuqxlq.mongodb.net/todoDB?retryWrites=true&w=majority&appName=Cluster0";
+// const uri = "mongodb+srv://mongodb:%40devsadiq6699@cluster0.2wuqxlq.mongodb.net/todoDB?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://mongodb:%40devsadiq6699@cluster0.2wuqxlq.mongodb.net/testDB?retryWrites=true&w=majority&appName=Cluster0";
 exports.client = new mongodb_1.MongoClient(uri, {
     serverApi: {
         version: mongodb_1.ServerApiVersion.v1,
--- M-3-Node.js/Todo-app/src/db/config.ts
+++ M-3-Node.js/Todo-app/src/db/config.ts
@@ -1,6 +1,7 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const uri = "mongodb+srv://mongodb:%40devsadiq6699@cluster0.2wuqxlq.mongodb.net/todoDB?retryWrites=true&w=majority&appName=Cluster0";
+// const uri = "mongodb+srv://mongodb:%40devsadiq6699@cluster0.2wuqxlq.mongodb.net/todoDB?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://mongodb:%40devsadiq6699@cluster0.2wuqxlq.mongodb.net/testDB?retryWrites=true&w=majority&appName=Cluster0";
 
  export const client = new MongoClient(uri, {
   serverApi: {
