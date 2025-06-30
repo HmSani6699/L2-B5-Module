f36ca3a684a4f0a08f50da0238497a119d0a9840
H.M Sadiq <hmsadikurrahman120@gmail.com>
2025-06-30 09:08:05 +0600

    created note route successfully

--- Assignmment-3/dist/App.js
+++ Assignmment-3/dist/App.js
@@ -1,11 +1,42 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = require("mongoose");
 exports.app = (0, express_1.default)();
+// Note Schema
+const noteSchema = new mongoose_1.Schema({
+    title: String,
+    content: String
+});
+// Note Schema Model
+const Note = (0, mongoose_1.model)('Note', noteSchema);
+// Create a new Note
+exports.app.post('/create-note', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const myNote = new Note({
+        title: "Note Number 1",
+        content: "My frist note in mongoose"
+    });
+    // Save data in mongodb database
+    yield myNote.save();
+    res.status(201).json({
+        succcess: true,
+        message: "Note created sucessfully ...!",
+        myNote: myNote
+    });
+}));
 exports.app.get('/', (req, res) => {
     res.send('Hallo world');
 });
--- Assignmment-3/src/App.ts
+++ Assignmment-3/src/App.ts
@@ -1,6 +1,36 @@
 import  express, { Application, Request, Response }  from "express";
+import { model, Schema } from "mongoose";
 
 export const app: Application = express();
+
+// Note Schema
+const noteSchema = new Schema({
+    title: String,
+    content: String
+});
+
+
+// Note Schema Model
+const Note = model('Note', noteSchema);
+
+
+// Create a new Note
+app.post('/create-note',async (req: Request, res: Response) => {
+    const myNote = new Note({
+        title: "Note Number 1",
+        content: "My frist note in mongoose"
+    });
+
+    // Save data in mongodb database
+    await myNote.save();
+
+    res.status(201).json({
+        succcess: true,
+        message: "Note created sucessfully ...!",
+        myNote:myNote
+    })
+
+})
 
 
 app.get('/', (req: Request, res: Response) => {
